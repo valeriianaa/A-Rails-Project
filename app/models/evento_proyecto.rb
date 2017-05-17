2db0e3f6b93310d3e9957d49cb56cffde974fc45
valeriianaa <valeriianaa@gmail.com>
2017-05-17 03:18:09 -0300

    Eventos > Fix Bugs

--- app/models/evento_proyecto.rb
+++ app/models/evento_proyecto.rb
@@ -2,7 +2,7 @@ class EventoProyecto < ActiveRecord::Base
 	belongs_to :evento
 	belongs_to :proyecto
 
-	validates :proyecto_id, :evento_id, presence: true
+	# validates :proyecto_id, :evento_id, presence: true
 	validates :proyecto_id, uniqueness: { scope: :evento_id, message: "El proyecto ya se encuentra asociado a este proyecto" }
 
 	audited
